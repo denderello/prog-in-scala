a843e5ee68c84e12a5a1463aa65a59a498b95064
Dennis Benkert <spinecrasher@googlemail.com>
2014-05-13 09:15:54 +0200

    added types to Application's val definitions

--- scala/chapter_10/Application.scala
+++ scala/chapter_10/Application.scala
@@ -1,9 +1,9 @@
-import foo.{ArrayElement,LineElement,UniformElement}
+import foo.{Element, ArrayElement,LineElement,UniformElement}
 
 object Application extends App {  
-  val elem = new ArrayElement(Array("hello", "world"))
-  val lineElem = new LineElement("hello world")
-  val uniformElem = new UniformElement('d', 50, 10)
+  val elem: Element = new ArrayElement(Array("hello", "world"))
+  val lineElem: ArrayElement = new LineElement("hello world")
+  val uniformElem: Element = new UniformElement('d', 50, 10)
   
   println(elem.width)
   println(lineElem.width)
