013c476deb8cc4d6a644b2ebf5639e3f7bd8e781
Dennis Benkert <spinecrasher@googlemail.com>
2014-05-13 09:13:26 +0200

    added UniformElement class

--- scala/chapter_10/Application.scala
+++ scala/chapter_10/Application.scala
@@ -1,9 +1,11 @@
-import foo.{ArrayElement,LineElement}
+import foo.{ArrayElement,LineElement,UniformElement}
 
 object Application extends App {  
   val elem = new ArrayElement(Array("hello", "world"))
   val lineElem = new LineElement("hello world")
+  val uniformElem = new UniformElement('d', 50, 10)
   
   println(elem.width)
   println(lineElem.width)
+  println(uniformElem.width)
 }
