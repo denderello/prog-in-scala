84bfa7b750df68cd9d1f027f6c703a57916846dc
Dennis Benkert <spinecrasher@googlemail.com>
2014-05-13 09:04:36 +0200

    added LineElement class

--- scala/chapter_10/Application.scala
+++ scala/chapter_10/Application.scala
@@ -1,7 +1,9 @@
-import foo.ArrayElement
+import foo.{ArrayElement,LineElement}
 
 object Application extends App {  
   val elem = new ArrayElement(Array("hello", "world"))
+  val lineElem = new LineElement("hello world")
   
   println(elem.width)
+  println(lineElem.width)
 }
